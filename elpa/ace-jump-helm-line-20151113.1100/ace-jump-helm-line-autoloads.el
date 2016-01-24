0f8470c1b89287a662e58695a945dc8391883de8
张彬 <zhangbin_1012@icloud.com>
2016-01-24 15:05:13 +0800

    reset

--- elpa/ace-jump-helm-line-20151113.1100/ace-jump-helm-line-autoloads.el
+++
@@ -1,16 +0,0 @@
-;;; ace-jump-helm-line-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("ace-jump-helm-line.el") (22179 55138
-;;;;;;  165788 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; ace-jump-helm-line-autoloads.el ends here
--- elpa/package-build-20160122.1405/package-build-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "package-build" "20160122.1405" "Tools for assembling a package archive" '((cl-lib "0.5")) :keywords '("tools"))
